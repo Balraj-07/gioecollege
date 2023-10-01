696eccb19dd1f6ee6f7a1c7691d01748cf3d8470
Ashokchittari <107450025+Ashokchittari@users.noreply.github.com>
2023-10-01 14:40:09 +0530

    Update index.js

--- src/Components/Loginpage/index.js
+++ src/Components/Loginpage/index.js
@@ -1,27 +1,29 @@
 import './index.css';
 
-const Loginpage = () =>{
+const Header = () =>{
     const ram = " "
     console.log(ram)
     return(
-        <div className="headCard m-5">
+        <div className="headCard">
+            <div className='clg-logo-container'>
             <div>
                 <img src="https://play-lh.googleusercontent.com/cP3rXN_KOucFNP8QrftBS2uoiT-hHH1BqRoLUwltJMC3MPQKwXFzIML-21Kwenxb3eY" alt="logo" className="image" />
             </div>
             <div>
-                <h1 className="text">Government Institute Of Electronics</h1>
-                <h5 className="text_low"> Government college in Secunderabad, Telangana</h5>
+                <h1 className="text-center clg-name-heading">Government Institute Of Electronics</h1>
+                <h5 className="text-center clg-address-heading"> Government college in Secunderabad, Telangana</h5>
             </div>
-            <div>
-                <h5 className="mb-5 text">Student/Parent Login</h5>
-                <div className="headCard">
-                    <input placeholder="LoginID" className="form-control mr-2 "/>
-                    <input placeholder="********" className="form-control "/>
-                    <button className="btn-primary ml-2 button">Login</button>
+            </div>
+            <div className='login-main-container'>
+                <h5 className="mb-3 text">Student/Parent Login</h5>
+                <div className="login-form">
+                    <input placeholder="LoginID" className="form-control"/>
+                    <input placeholder="********" className="form-control login-margin-1"/>
+                    <button className="btn-primary ml-2 button login-margin-1 text-center">Login</button>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Loginpage
+export default Header
