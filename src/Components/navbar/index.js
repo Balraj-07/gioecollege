999e8f21c928a18c87a805e940c91a09fc999fc2
Ashokchittari <107450025+Ashokchittari@users.noreply.github.com>
2023-10-01 14:41:39 +0530

    Update index.js

--- src/Components/navbar/index.js
+++ src/Components/navbar/index.js
@@ -4,16 +4,16 @@ const Navbar = () =>{
     const ram = ""
 
     return(
-        <div style={{backgroundColor:"blue"}}>
-        <nav className="navbar navbar-expand-lg" style={{backgroundColor:"blue"}}>  
+        <div className='bg-primary'>
+        <nav className="navbar navbar-expand-lg navbar-light justify-content-end mt-3 navbar-hamburger" style={{backgroundColor:"blue"}}>  
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse flexnav" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto" >
-            <li className="nav-item active ml-5">
-                <a className="nav-link ml-5" href="#" style={{color:'white'}}>HOME <span class="sr-only">(current)</span></a>
+            <li className="nav-item active">
+                <a className="nav-link ml-2" href="#" style={{color:'white'}}>HOME <span class="sr-only">(current)</span></a>  
             </li>
             <li className="nav-item">
                 <a className="nav-link ml-2" href="#" style={{color:'white'}}>ABOUT</a>
